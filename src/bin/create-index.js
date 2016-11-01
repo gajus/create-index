9278b25dcd38cb9e979d7150b77041117c35e149
Gajus Kuizinas <gajus@gajus.com>
2016-11-01 15:50:02 +0000

    feat: rename --update-index to --update (fixes #10)

--- src/bin/create-index.js
+++ src/bin/create-index.js
@@ -8,14 +8,14 @@ import {
 const argv = yargs
   .demand(1)
   .options({
-    'update-index': {
+    'update': {
       default: false,
       description: 'Recursively iterates target directories looking for "index.js" files that start with "\'create index\';\\n" (create-index index file). Updates found index files. Does not create new index files.',
       type: 'boolean'
     }
   })
   .example('create-index ./src ./src/utilities', 'Creates or updates an existing create-index index file in the target (./src, ./src/utilities) directories.')
-  .example('create-index --update-index ./src ./tests', 'Finds all create-index index files in the target directories and descending directories. Updates found index files.')
+  .example('create-index --update ./src ./tests', 'Finds all create-index index files in the target directories and descending directories. Updates found index files.')
   .argv;
 
 writeIndexCli(argv._, {
