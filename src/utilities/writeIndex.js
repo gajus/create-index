5b496536c64ea9542b5e5f0a09f50c77798a3a52
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-09-27 20:06:46 +0000

    update and fixes

--- src/utilities/writeIndex.js
+++ src/utilities/writeIndex.js
@@ -10,15 +10,15 @@ import sortByDepth from './sortByDepth';
 export default (directoryPaths, options = {}) => {
   const sortedDirectoryPaths = sortByDepth(directoryPaths)
     .filter((directoryPath) => {
-      return validateTargetDirectory(directoryPath, {silent: options.ignoreUnsafe});
+      return validateTargetDirectory(directoryPath, {silent: options.ignoreUnsafe, outputFile: options.outputFile});
     });
 
   _.forEach(sortedDirectoryPaths, (directoryPath) => {
-    const config = readIndexConfig(directoryPath);
+    const config = readIndexConfig(directoryPath, options);
     const optionsWithConfig = Object.assign({}, options, {config});
     const siblings = readDirectory(directoryPath, optionsWithConfig);
     const indexCode = createIndexCode(siblings, optionsWithConfig);
-    const indexFilePath = path.resolve(directoryPath, optionsWithConfig.outputFile || 'index.js');
+    const indexFilePath = path.resolve(directoryPath, options.outputFile || 'index.js');
 
     fs.writeFileSync(indexFilePath, indexCode);
   });
